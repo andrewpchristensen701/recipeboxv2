d297c10c54257123fe0a119bc5ebd9d5b7eb829e
Unknown <keybladeschoosen1@gmail.com>
2019-11-17 18:19:38 -0500

    added login form

--- recipeBox/forms.py
+++ recipeBox/forms.py
@@ -12,3 +12,8 @@ class CreateRecipeForm(forms.Form):
     author = forms.ModelChoiceField(queryset=Author.objects.all())
     time_required = forms.CharField(max_length=90)
     description = forms.CharField(widget=forms.Textarea)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput())
