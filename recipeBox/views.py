4407981225e091beb3bac00fa43f2adaa406b1fa
Unknown <keybladeschoosen1@gmail.com>
2019-11-17 18:23:53 -0500

    updated views.py

--- recipeBox/views.py
+++ recipeBox/views.py
@@ -1,27 +1,42 @@
+from .forms import CreateAuthorForm, CreateRecipeForm, LoginForm, CreatUserForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from recipeBox.models import Author, Recipe
-from .forms import CreateAuthorForm, CreateRecipeForm
-from django.contrib.auth.models import User
 
 
 def recipes(request, **kwargs):
     all_recipes = Recipe.objects.all()
-    return render(request, 'index.html', {'recipes': all_recipes})
+    status = 'login' if not request.user.is_authenticated else 'logout'
+
+    return render(request,
+                  'index.html',
+                  {'recipes': all_recipes,
+                   'sign_in_button_label': status,
+                   'sign_in_button_link': reverse(status)
+                   })
 
 
 def recipe(request, recipe_id):
     _recipe = Recipe.objects.get(id=recipe_id)
-    return render(request, 'recipe.html', {'recipe': _recipe})
+    return render(request, 'recipe.html',
+                  {
+                      'recipe': _recipe
+                  })
 
 
 def author(request, author_name):
     _author = Author.objects.get(name=author_name)
     _recipes = Recipe.objects.filter(author=_author)
-    return render(request, 'author.html', {'author': _author, 'recipes': _recipes})
+    return render(request, 'author.html',
+                  {'author': _author,
+                   'recipes': _recipes
+                   })
 
 
+@login_required()
 def new_author(request, **kwargs):
-    html = "create_author.html"
 
     if request.method == "POST":
         form = CreateAuthorForm(request.POST)
@@ -37,16 +52,16 @@ def new_author(request, **kwargs):
                 bio=data['bio']
             )
             return HttpResponseRedirect(reverse('homepage'))
-
     form = CreateAuthorForm()
-    return render(request, html, {'form': form})
+    return render(request, "create_author.html", {'form': form})
 
 
+@login_required()
 def new_recipe(request):
     html = 'create_recipe.html'
 
     if request.method == 'POST':
-        form = CreateRecipeForm(request.POST)
+        form = CreateRecipeForm(request.user, request.POST)
 
         if form.is_valid():
             data = form.cleaned_data
@@ -62,3 +77,56 @@ def new_recipe(request):
     form = CreateRecipeForm()
     return render(request, html, {'form': form})
 
+
+def signup_view(request):
+    html = 'generic_form.html.j2'
+    form = None
+
+    if request.method == 'POST':
+        form = CreatUserForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            new_user = User.objects.create(
+                username=data['username'],
+                password=data['password']
+            )
+            Author.objects.create(
+                name=data['name'],
+                user=new_user
+            )
+            login(request, new_user)
+            return HttpResponseRedirect(reverse('home'))
+
+    else:
+        form = CreatUserForm()
+
+    return render(request, html, {'form': form})
+
+
+def login_view(request):
+    html = 'login.html'
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['username'],
+                password=data['password']
+            )
+
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('homepage'))
+                )
+    else:
+        form = LoginForm()
+    return render(request, html, {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
