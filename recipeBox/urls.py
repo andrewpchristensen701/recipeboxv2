629814aeeeac93d451f1d5cc24d07a330d9ca6e6
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-12-10 14:51:14 -0500

    finish

--- recipeBox/urls.py
+++ recipeBox/urls.py
@@ -20,11 +20,14 @@ from recipeBox import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.recipes, name="homepage"),
-    path('author/<str:author_name>', views.author),
+    path('author/<str:author_name>', views.author, name="author"),
     path('recipe/<int:recipe_id>', views.recipe),
     path('newAuthor/', views.new_author, name="create author"),
     path('newRecipe/', views.new_recipe, name="create recipe"),
     path('newUser/', views.signup_view, name='create user'),
     path('login/', views.login_view, name="login"),
-    path('logout/', views.logout_view, name="logout")
+    path('logout/', views.logout_view, name="logout"),
+    path('editrecipe/<int:recipe_id>', views.edit_recipe, name="edit"),
+    path('favorite/<int:id>/', views.favoriterecipe),
+    path('unfavorite/<int:id>/', views.unfavoriterecipe),
 ]
